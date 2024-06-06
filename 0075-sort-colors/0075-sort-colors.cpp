766c6a3de47da9c914d83cfbc8b289c7e24fcd3d
Mahi Chrungoo <98683515+MAHIC-1201@users.noreply.github.com>
2024-06-06 17:11:31 +0530

    Time: 0 ms (100%), Space: 9.6 MB (95.18%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -2,21 +2,15 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int length=nums.size();
-        int temp;
-        while(length!=1)
-        {
-            for(int i=0;i<length-1;i++)
-            {
-                if(nums[i]>nums[i+1])
-                {
-                    temp=nums[i];
-                    nums[i]=nums[i+1];
-                    nums[i+1]=temp;
+        int n=nums.size();
+        while(length!=1){
+            for(int i=0;i<n-1;i++){
+                if(nums[i]>nums[i+1]){
+                    swap(nums[i], nums[i+1]);
                 }
             }
             length--;
         }
-        
-        
+    
     }
 };
