1e42e96e42b18c52e10f6b75ab96e2cc5b2c94a0
Mahi Chrungoo <98683515+MAHIC-1201@users.noreply.github.com>
2024-06-07 18:43:19 +0530

    Time: 7 ms (85.2%), Space: 13.1 MB (51.94%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,18 +1,37 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& a, int target) {
-        int n=a.size();
-        for(int i=0;i<n;i++){
-            int sum=0;
-            for(int j=i+1;j<n;j++){
-                sum=a[i]+a[j];
-                if(sum==target){
-                    return {i,j};
-                }
-
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int>arr2(nums.size());
+        copy(nums.begin(), nums.end(), arr2.begin());
+        int n=nums.size();
+        int i=0, j=n-1;
+        sort(nums.begin(), nums.end());
+        int num1,num2;
+        while(i<j){
+            if(nums[i]+nums[j]==target){
+                num1=nums[i];
+                num2=nums[j];
+                break;
+            }
+            else if(nums[i]+nums[j]<target){
+                i++;
+            }
+            else{
+                j--;
             }
         }
-        return {};
+        cout<<num1<<num2<<endl;
+        int indx1=0, indx2=0;
+        for(int i=0;i<n;i++){
+            if(arr2[i]==num1 && indx1==0){
+                indx1=i;
+            }
+            else if(arr2[i]==num2){
+                indx2=i;
+            }
+        }
+        return {indx1, indx2};
+
           
     }
 };
